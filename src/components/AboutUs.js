41eb9963e1300b876fd0d92cc74faee015e2e18b
tomyferra <ferra.tomy@gmail.com>
2023-03-02 11:42:09 -0300

    edited the navbar

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -12,9 +12,11 @@ function AboutUs() {
             {/* <img class="card-img-start rounded img-fluid" src={Logo} alt="wine cap" /> */}
             <h1 className="text-about-us">Estudio Vivanco</h1>
             <h4 className="text-about-us">Liquidadores de Averías - Peritos - Consultores</h4>
-            <h5 class="text-justify">Fundada 1978, dedicados al ajuste de siniestros relativos a cascos y al envió de carga, vía aérea, terrestre, marítima o fluvial.</h5>
-            {/* <h5 className="text-justify">En la actualidad hay dos socios, los cuales participan activamente en el funcionamiento diario de la oficina. Según lo exige la legislación argentina, cada socio está autorizado por la Superintendencia Nacional de Seguros como Ajustador de Averías reconocido.</h5> */}
-            <h5 className="text-justify">Desde 1999, ambos Socios son Miembros de la <b>“Association Mondiale de Dispacheurs”</b> (Asociación Internacional de Ajustadores de Averías - ex <b>A.I.D.E.</b>)</h5>
+            {/* <h5 class="text-justify">Fundada 1978, dedicados al ajuste de siniestros relativos a cascos y al envió de carga, vía aérea, terrestre, marítima o fluvial.</h5> */}
+            <h5 class="text-justify">Fundada 1978, dedicados al peritajes y liquidación en siniestros de transportes (Marítimos, Aéreos, Terrestres, Fluviales).</h5>
+            {/* <h5 class="text-justify">Liquidación de Siniestros de Cascos</h5>
+            <h5 class="text-justify">Averías Gruesas</h5> */}
+            <h5 className="text-justify">Desde 1999, los Socios son Miembros de la <b>“Association Mondiale de Dispacheurs”</b> (Asociación Internacional de Ajustadores de Averías - ex <b>A.I.D.E.</b>)</h5>
           </div>
 
         </div>
